6f39d53e239461599599a616d6fb79f7d88ebe03
Dan Butvinik <dan.butvinik@gmail.com>
2024-05-27 15:22:15 -0400

    find current index ratio and calculate the current adjusted principal
    - add input original principal
    - calculate current adjusted principal
    - use fecha to format date strings
    - highlight the current date in the table in yellow

--- components/cusipDetails.js
+++ components/cusipDetails.js
@@ -1,16 +1,26 @@
 'use client'
 
+import { format } from 'fecha'
 import { useEffect, useState } from 'react'
 import { getCpiEntries } from '../actions/treasuryApi'
 import styles from '../styles/CusipDetails.module.css'
 
-export default function CusipDetails({ cusip }) {
+export default function CusipDetails({ cusip, originalPrincipal }) {
     const [cpiEntries, setCpiEntries] = useState(null)
+    const [currentCpiEntry, setCurrentCpiEntry] = useState(null)
+    const [adjustedPrincipal, setAdjustedPrincipal] = useState(null)
 
     useEffect(() => {
         const fetchData = async () => {
             let result = await getCpiEntries(cusip)
             setCpiEntries(result)
+
+            // find today's entry by matching the current date with the indexDate
+            let todaysEntry = result.find(
+                (entry) => entry.indexDate == format(new Date(), 'YYYY-MM-DDT00:00:00'),
+            )
+            setCurrentCpiEntry(todaysEntry)
+            setAdjustedPrincipal((todaysEntry?.dailyIndex * originalPrincipal).toFixed(2))
         }
         if (cusip) {
             fetchData().catch(console.error)
@@ -19,7 +29,10 @@ export default function CusipDetails({ cusip }) {
 
     return (
         <div className={styles.cusipDetails}>
-            {cusip && <p>CUSIP: {cusip}</p>}
+            <p>CUSIP: {cusip}</p>
+            <p>Original Principal: ${originalPrincipal}</p>
+            <p>Current Index Ratio: {Number(currentCpiEntry?.dailyIndex)}</p>
+            <p>Current Adjusted Principal: ${adjustedPrincipal}</p>
             {cpiEntries && (
                 <div className={styles.cpiEntries}>
                     <table>
@@ -31,7 +44,14 @@ export default function CusipDetails({ cusip }) {
                         </thead>
                         <tbody>
                             {cpiEntries?.map((entry) => (
-                                <tr key={entry.uniqueKey}>
+                                <tr
+                                    key={entry.uniqueKey}
+                                    className={
+                                        entry.dailyIndex == currentCpiEntry?.dailyIndex
+                                            ? styles.current
+                                            : ''
+                                    }
+                                >
                                     <td>{new Date(entry.indexDate).toLocaleDateString()}</td>
                                     <td>{entry.dailyIndex}</td>
                                 </tr>
