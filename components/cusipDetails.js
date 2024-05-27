002f2d3c9ae09cacc0faf9732581ce862fa1102b
Dan Butvinik <dan.butvinik@gmail.com>
2024-05-27 16:42:17 -0400

    chart the dailyIndex data over time
    - install recharts
    - format security details into a table
    - add the line chart
    - start showing daily adjusted principal in the cpi entries

--- components/cusipDetails.js
+++ components/cusipDetails.js
@@ -1,6 +1,8 @@
 'use client'
 
 import { format } from 'fecha'
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts'
+
 import { useEffect, useState } from 'react'
 import { getCpiEntries, getSecurityDetails } from '../actions/treasuryApi'
 import styles from '../styles/CusipDetails.module.css'
@@ -10,6 +12,8 @@ export default function CusipDetails({ cusip, originalPrincipal }) {
     const [securityDetails, setSecurityDetails] = useState(null)
     const [currentCpiEntry, setCurrentCpiEntry] = useState(null)
     const [adjustedPrincipal, setAdjustedPrincipal] = useState(null)
+    const [cpiChartData, setCpiChartData] = useState(null)
+    const [isLoading, setIsLoading] = useState(true)
 
     useEffect(() => {
         const fetchData = async () => {
@@ -25,22 +29,87 @@ export default function CusipDetails({ cusip, originalPrincipal }) {
             )
             setCurrentCpiEntry(todaysEntry)
             setAdjustedPrincipal((todaysEntry?.dailyIndex * originalPrincipal).toFixed(2))
+
+            setCpiChartData(
+                cpiEntriesResponse
+                    .map((entry) => {
+                        return {
+                            indexDate: new Date(entry.indexDate).toLocaleDateString(),
+                            dailyIndex: Number(entry.dailyIndex),
+                            dailyAdjustedPrincipal: Number(
+                                originalPrincipal * entry.dailyIndex,
+                            ).toFixed(2),
+                        }
+                    })
+                    .reverse(),
+            )
         }
         if (cusip) {
-            fetchData().catch(console.error)
+            fetchData()
+                .catch(console.error)
+                .finally(() => {
+                    setIsLoading(false)
+                })
         }
     }, [cusip])
 
+    if (isLoading) {
+        return <div>Loading ...</div>
+    }
+
     return (
         <div className={styles.cusipDetails}>
-            <p>CUSIP: {cusip}</p>
-            <p>Current Adjusted Principal: ${adjustedPrincipal}</p>
-            <p>Original Principal: ${originalPrincipal}</p>
-            <p>Current Index Ratio: {Number(currentCpiEntry?.dailyIndex)}</p>
-            <p>Security Term: {securityDetails?.securityTerm}</p>
-            <p>Issue Date: {new Date(securityDetails?.issueDate).toLocaleDateString()}</p>
-            <p>Maturity Date: {new Date(securityDetails?.maturityDate).toLocaleDateString()}</p>
-            <p>Interest Rate: {Number(securityDetails?.interestRate).toFixed(2)}%</p>
+            <table>
+                <tbody>
+                    <tr>
+                        <td>CUSIP:</td>
+                        <td>{cusip}</td>
+                    </tr>
+                    <tr>
+                        <td>Current Adjusted Principal:</td>
+                        <td>${adjustedPrincipal}</td>
+                    </tr>
+                    <tr>
+                        <td>Original Principal:</td>
+                        <td>${originalPrincipal}</td>
+                    </tr>
+                    <tr>
+                        <td>Current Index Ratio:</td>
+                        <td>{Number(currentCpiEntry?.dailyIndex)}</td>
+                    </tr>
+                    <tr>
+                        <td>Security Term:</td>
+                        <td>{securityDetails?.securityTerm}</td>
+                    </tr>
+                    <tr>
+                        <td>Issue Date:</td>
+                        <td>{new Date(securityDetails?.issueDate).toLocaleDateString()}</td>
+                    </tr>
+                    <tr>
+                        <td>Maturity Date:</td>
+                        <td>{new Date(securityDetails?.maturityDate).toLocaleDateString()}</td>
+                    </tr>
+                    <tr>
+                        <td>Interest Rate:</td>
+                        <td>{Number(securityDetails?.interestRate).toFixed(2)}%</td>
+                    </tr>
+                </tbody>
+            </table>
+
+            {cpiEntries && (
+                <LineChart
+                    width={600}
+                    height={300}
+                    data={cpiChartData}
+                    margin={{ top: 20, right: 10, bottom: 10, left: 10 }}
+                >
+                    <Line type="monotone" dataKey="dailyIndex" stroke="#8884d8" />
+                    <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                    <XAxis dataKey="indexDate" />
+                    <YAxis type="number" />
+                    <Tooltip />
+                </LineChart>
+            )}
 
             {cpiEntries && (
                 <div className={styles.cpiEntries}>
@@ -49,6 +118,7 @@ export default function CusipDetails({ cusip, originalPrincipal }) {
                             <tr>
                                 <th>Date</th>
                                 <th>Index Ratio</th>
+                                <th>Daily Adjusted Principal</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -63,6 +133,7 @@ export default function CusipDetails({ cusip, originalPrincipal }) {
                                 >
                                     <td>{new Date(entry.indexDate).toLocaleDateString()}</td>
                                     <td>{entry.dailyIndex}</td>
+                                    <td>{`$${Number(entry.dailyIndex * originalPrincipal).toFixed(2)}`}</td>
                                 </tr>
                             ))}
                         </tbody>
